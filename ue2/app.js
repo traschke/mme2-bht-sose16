62e50a0c6cd377be235ff4d63aecad41ec9b0e1e
Ibrahim Khaled Reguieg <khaled.reguieg@gmail.com>
2016-04-20 16:27:00 +0200

    ADD Uebung2 Aufgabe1zeigt website mit helloWorld auf localhost:3000

--- ue2/app.js
+++ ue2/app.js
@@ -1,11 +1,19 @@
-//node app.js in Console aufrufen --->  http://localhost:3000/ in Browser
+/**
+ * Created by Timo Raschke 812538 <a href="timo.raschke@gmail.com">Timo Raschke"</a>, Khaled Reguieg 813812, Pauline Schmiechen 813874 on 20.04.2016.
+ * This file represents the entrypoint of our application.
+ */
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('<!DOCTYPE html>' +
+      '<html lane="en"' +
+      '<head><meta charset="utf-8"></head>' +
+      '<body><h1>Hello World!</h1></body>' +
+      '</html>'
+  );
 });
 
-app.listen(3000, function () {
-  console.log('"Hallo World" app listening on port 3000!');
-});
+var server = app.listen(3000, function () {
+  console.log("helloworld app is ready and listening at http://localhost:3000");
+})
