abb4927bfe894c09174b7c7604c261897ca6dd99
Pauline Schmiechen <schmiechenpauline@gmail.com>
2016-06-16 14:45:52 +0200

    alles funkt außer put

--- ue5/app.js
+++ ue5/app.js
@@ -5,7 +5,7 @@
  * NODE_ENV=development
  * DEBUG=me2*
  *
- * @author Johannes Konert
+ * @author Johannes Konert, Pauline Schmiechen
  * @licence CC BY-SA 4.0
  *
  */
@@ -18,13 +18,16 @@ var bodyParser = require('body-parser');
 var requestLogger = require('morgan');
 var debug = require('debug')('me2u5:server');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 // own modules
 var restAPIchecks = require('./restapi/request-checks.js');
 var errorResponseWare = require('./restapi/error-response');
 
 var videos = require('./routes/videos');
-
+//mogo db anbindung
+var db = mongoose.connect('mongodb://localhost:27017/me2');
+var videoModel = require('./models/video');
 
 
 // app creation
@@ -44,9 +47,78 @@ app.use(restAPIchecks);
 
 
 // Routes ******************************************************
-app.use('/videos', videos);
+//app.use('/videos', videos);
 
+app.route('/videos')
+    .get(function(req, res, next){
+        videoModel.find(function(err, items) {
+            res.json(items);
+        });
+    })
+    .post(function(req, res, next) {
+        var video = new videoModel(req.body);
+        video.save(function(err) {
+            if (!err) { res.status(201).json(video)
+            }else {
+                next(err);
+            }
+        });
+    })
+    .delete(function (req, res, next) {
+        res.status(404).end();
+    })
+    .put(function (req, res, next) {
+        var err = new Error('{"error": { "message": "Wrong Content-Type.", "code": 405 } }');
+        err.status = 405;
+        next(err);
+    });
 
+app.route('/videos/:id')
+    .get(function(req, res, next){
+        videoModel.findOne({
+        '_id' : req.params.id
+        }, function (err, items) {
+            res.json(items);
+        })
+    })
+    .post(function (req, res, next) {
+        var err = new Error('{"error": { "message": "This is the wrong URL you are sending your POST to.", "code": 400 } }');
+        err.status = 405;
+        next(err);
+    })
+    .delete(function(req, res, next){
+        videoModel.findOne({
+            '_id' : req.params.id
+        }, function (err, item) {
+           item.remove();
+            res.json(item);
+        })
+    })
+    .patch(function (req, res, next) {
+        videoModel.findByIdAndUpdate(req.params.id, req.body, function (err){
+            if(err)
+                next(err);
+            res.json({ message: 'Video updated!' });
+
+        });
+    })
+    .put(function (req, res, next) {
+        videoModel.findById(req.params.id, function(err, video) {
+            if (!video)
+               next(err);
+            else {
+                // do your updates here
+                video.title = req.body.title;
+
+                video.save(function(err) {
+                    if (err)
+                        console.log('error')
+                    else
+                        console.log('success')
+                });
+            }
+        });
+    });
 
 
 
