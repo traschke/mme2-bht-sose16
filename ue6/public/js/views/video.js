fcdfc5dc5ac69f30433e3857fd1167100721f837
Timo Raschke <timo.raschke@gmail.com>
2016-06-27 15:04:09 +0200

    Aufgabe Bonus 3: - Buttons are working - video ended event doesn't get triggered...

--- ue6/public/js/views/video.js
+++ ue6/public/js/views/video.js
@@ -11,6 +11,38 @@ define(["backbone", "jquery", "underscore"], function(Backbone, $,  _){
         },
         initialize : function () {
             this.listenTo(this.model, "change", this.render);
+        },
+        events: {
+            'click .play': 'playVideo',
+            'click .pause': 'pauseVideo',
+            'click .stop': 'stopVideo',
+            'ended .video': 'videoEnded'
+        },
+        playVideo: function () {
+            this.$el.find('video').get(0).play();
+            this.$el.find('.play').attr('disabled', true);
+            this.$el.find('.pause').attr('disabled', false);
+            this.$el.find('.stop').attr('disabled', false);
+        },
+        pauseVideo: function () {
+            this.$el.find('video').get(0).pause();
+            this.$el.find('.play').attr('disabled', false);
+            this.$el.find('.pause').attr('disabled', true);
+            this.$el.find('.stop').attr('disabled', false);
+        },
+        stopVideo: function () {
+            this.$el.find('video').get(0).pause();
+            this.$el.find('video').get(0).currentTime = 0.0;
+            this.$el.find('.play').attr('disabled', false);
+            this.$el.find('.pause').attr('disabled', true);
+            this.$el.find('.stop').attr('disabled', true);
+        },
+        videoEnded: function () {
+            console.log('FIRE');
+            this.$el.find('video').get(0).currentTime = 0.0;
+            this.$el.find('.play').attr('disabled', false);
+            this.$el.find('.pause').attr('disabled', true);
+            this.$el.find('.stop').attr('disabled', true);
         }
     });
     return VideoView;
